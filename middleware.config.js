c5135d2263016d70d752273bee0055942182ef19
Mark Brocato <mark.brocato@moovweb.com>
2021-06-11 14:24:13 +0300

    working without layer0

--- middleware.config.js
+++ middleware.config.js
@@ -4,8 +4,8 @@ module.exports = {
       location: '@vue-storefront/shopify-api/server',
       configuration: {
         api: {
-          domain: '<DOMAIN>',
-          storefrontAccessToken: '<TOKEN>'
+          domain: 'puredailycare.com',
+          storefrontAccessToken: '990e49e1737e0593cadee15ede4b2972'
         },
         currency: 'USD',
         country: 'US'
